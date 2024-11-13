f060f3987b10cb91271c40b5dfb0c4de13202192
Rochi Eko Pambudi <rochieko11@gmail.com>
2024-11-14 06:47:43 +0700

    CI file

--- testing/test.py
+++ testing/test.py
@@ -17,6 +17,6 @@ def get_title_from_web(url):
 
 web_url = "http://localhost:5000"
 title = get_title_from_web(web_url)
-print(title)
+# print(title)
 assert title == "Form Absensi Mahasiswa"
 print("Testing success")
